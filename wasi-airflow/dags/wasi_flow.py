2d20809e968b490544434b6e788045e1a50afa4a
Carl <luohuazju@gmail.com>
2023-03-27 23:08:01 -0500

    add as build

--- wasi-airflow/dags/wasi_flow.py
+++ wasi-airflow/dags/wasi_flow.py
@@ -34,6 +34,9 @@ fetch_poc_from_github = BashOperator(
     dag=dag,
 )
 
+########################################
+# Rust App
+########################################
 prepare_rust_dependency = BashOperator(
     task_id='prepare_rust_dependency',
     depends_on_past=False,
@@ -50,4 +53,25 @@ build_app_with_rust = BashOperator(
     dag=dag,
 )
 
+#############################################
+# AssemblyScript App
+#############################################
+prepare_as_dependency = BashOperator(
+    task_id='prepare_as_dependency',
+    depends_on_past=False,
+    bash_command='cd /home/carl/work/sillycat-wasm-solution/wasi-consumer-as/ && '
+                 '~/.nodenv/shims/npm install ',
+    dag=dag,
+)
+
+build_app_with_as = BashOperator(
+    task_id='build_app_with_as',
+    depends_on_past=False,
+    bash_command='cd /home/carl/work/sillycat-wasm-solution/wasi-consumer-as/ && '
+                 '~/.nodenv/shims/npm run build:release ',
+    dag=dag,
+)
+
+
 fetch_poc_from_github >> prepare_rust_dependency >> build_app_with_rust
+fetch_poc_from_github >> prepare_as_dependency >> build_app_with_as
