3f25d731365845286c9862bc5efa33fbecbb0836
Carl <luohuazju@gmail.com>
2023-05-15 09:59:24 -0500

    ENV working fine

--- wasi-airflow-docker/dags/wasi_flow.py
+++ wasi-airflow-docker/dags/wasi_flow.py
@@ -71,4 +71,87 @@ unittest_app_with_rust = BashOperator(
     dag=dag,
 )
 
+#############################################
+# AssemblyScript App
+#############################################
+prepare_as_dependency = BashOperator(
+    task_id='prepare_as_dependency',
+    depends_on_past=False,
+    bash_command="""
+        cd /opt/airflow/sillycat-wasm-solution/wasi-consumer-as/
+        /usr/bin/npm install 
+        """,
+    dag=dag,
+)
+
+build_app_with_as = BashOperator(
+    task_id='build_app_with_as',
+    depends_on_past=False,
+    bash_command="""
+        cd /opt/airflow/sillycat-wasm-solution/wasi-consumer-as
+        /usr/bin/npm run build:release 
+    """,
+    dag=dag,
+)
+
+unittest_app_with_as = BashOperator(
+    task_id='unittest_app_with_as',
+    depends_on_past=False,
+    bash_command="""
+        cd /opt/airflow/sillycat-wasm-solution/wasi-consumer-as
+        /usr/bin/npm run test 
+    """,
+    dag=dag,
+)
+
+######################################
+# Prepare Runtime to Execute App
+######################################
+
+build_runtime = BashOperator(
+    task_id='build_runtime',
+    depends_on_past=False,
+    bash_command="""
+        cd /opt/airflow/sillycat-wasm-solution/wasi-impl/
+        ~/.cargo/bin/cargo build 
+        """,
+    dag=dag,
+)
+
+unittest_runtime = BashOperator(
+    task_id='unittest_runtime',
+    depends_on_past=False,
+    bash_command="""
+        cd /opt/airflow/sillycat-wasm-solution/wasi-impl/
+        ~/.cargo/bin/cargo test 
+        """,
+    dag=dag,
+)
+
+test_with_rust_app = BashOperator(
+    task_id='test_with_rust_app',
+    depends_on_past=False,
+    bash_command="""
+    cd /opt/airflow/sillycat-wasm-solution/wasi-impl/
+    ./target/debug/wasi-impl ../wasi-consumer-rust/target/wasm32-wasi/debug/wasi_consumer_rust.wasm consume_add 1 2 | grep -Ev '3' && exit 1
+    exit 0
+    """,
+    dag=dag,
+)
+
+test_with_as_app = BashOperator(
+    task_id='test_with_as_app',
+    depends_on_past=False,
+    bash_command="""
+    cd /opt/airflow/sillycat-wasm-solution/wasi-impl/
+    ./target/debug/wasi-impl ../wasi-consumer-as/build/wasi-consumer-as.wasm consume_add 1 4 | grep -Ev '5' && exit 1
+    exit 0
+    """,
+    dag=dag,
+)
+
+
 fetch_poc_from_github >> prepare_rust_dependency >> build_app_with_rust >> unittest_app_with_rust
+fetch_poc_from_github >> prepare_as_dependency >> build_app_with_as >> unittest_app_with_as
+fetch_poc_from_github >> build_runtime >> unittest_runtime >> test_with_rust_app
+fetch_poc_from_github >> build_runtime >> unittest_runtime >> test_with_as_app
